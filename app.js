bd66ea123c4b911216209b1b41cb4f77bd36b6bb
Fran Martinez <martinezcoder@gmail.com>
2015-05-21 10:36:38 +0200

    hide and show elements

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
 (function(){
   var app = angular.module('store', [ ]);
+
+  app.controller('StoreController', function(){
+    this.product = gem;
+  });
+
+  var gem = {
+    name: 'Dodecahedron',
+    price: 2.95,
+    description: 'this is a description of the Dodecahedron product',
+    canPurchase: true,
+    soldOut: true
+  }
 })();
 
