3d160b008f54982967a2f6078dc1568ed5612286
Fran Martinez <martinezcoder@gmail.com>
2015-05-21 10:40:53 +0200

    ng-repeat director

--- app.js
+++ app.js
@@ -2,15 +2,22 @@
   var app = angular.module('store', [ ]);
 
   app.controller('StoreController', function(){
-    this.product = gem;
+    this.products = gems;
   });
 
-  var gem = {
-    name: 'Dodecahedron',
-    price: 2.95,
-    description: 'this is a description of the Dodecahedron product',
-    canPurchase: true,
-    soldOut: true
-  }
+  var gems = [
+    {
+      name: 'Dodecahedron',
+      price: 2.95,
+      description: 'this is a description of the Dodecahedron product',
+      canPurchase: true,
+    },
+    {
+      name: "Personal Gem",
+      price: 5.95,
+      description: "this is another description",
+      canPurchase: false,
+    }
+  ];
 })();
 
