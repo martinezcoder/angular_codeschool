07d93f553da10b1be2fba617835ff01b1cb831b0
Fran Martinez <martinezcoder@gmail.com>
2015-05-21 11:48:09 +0200

    show images

--- app.js
+++ app.js
@@ -11,12 +11,24 @@
       price: 2,
       description: 'this is a description of the Dodecahedron product',
       canPurchase: true,
+      images: [
+        {
+          full: 'image1.png',
+          thumb: 'image1-thumb.png'
+        }
+      ]
     },
     {
       name: "Personal Gem",
       price: 5.95,
       description: "this is another description",
       canPurchase: false,
+      images: [
+        {
+          full: 'image2.png',
+          thumb: 'image2-thumb.png'
+        }
+      ]
     }
   ];
 })();
